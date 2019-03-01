6632d742d09a0f002a683863429caae26c533b06
Iván López <lopez.ivan@gmail.com>
2019-03-01 16:38:06 +0100

    Add hello controller

---
+++ src/main/java/micronaut/jaeger/graal/HelloController.java
@@ -0,0 +1,16 @@
+package micronaut.jaeger.graal;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.tracing.annotation.NewSpan;
+
+@Controller("/")
+public class HelloController {
+
+    @NewSpan
+    @Get("/hello/{name}")
+    public String sayHi(String name) {
+        return "Hello " + name;
+    }
+
+}
